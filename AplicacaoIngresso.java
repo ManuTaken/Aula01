3e28d256995a3a591772b19ec362e4161fe5e10e
ManuTaken <32471334+ManuTaken@users.noreply.github.com>
2017-10-20 16:07:55 -0200

    Create AplicacaoIngresso.java

---
+++ AplicacaoIngresso.java
@@ -0,0 +1,47 @@
+# Aula01
+
+public class AplicacaoIngresso {
+
+
+
+    public static void main(String[] args) {
+
+       
+
+        Ingresso a= new Ingresso();
+
+        Vip b= new Vip();
+
+        Normal c = new Normal();
+
+        CamaroteInferior d =new CamaroteInferior();
+
+        CamaroteSuperior e = new CamaroteSuperior();
+
+        
+
+        
+
+        
+
+        a.imprimeValor();
+
+        b.valorAdicional();
+
+        c.valorNormal();
+
+        d.camaroteInf();
+
+        e.camaroteSup();
+
+        
+
+        
+
+        
+
+    }
+
+    
+
+}
