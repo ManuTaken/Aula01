e9f178ab5a60e2cba8d32708a2028cf0b76d4d45
ManuTaken <32471334+ManuTaken@users.noreply.github.com>
2017-10-20 16:02:30 -0200

    Create Vip.java

---
+++ Vip.java
@@ -0,0 +1,37 @@
+# Aula01
+
+public class Vip extends Ingresso {
+
+    
+
+    float adicional=15;
+
+    float vip;
+
+    
+
+    public void valorAdicional()
+
+    {
+
+        this.vip= adicional + valor;
+
+        
+
+        System.out.println("Valor Vip: R$"+vip);
+
+    
+
+    
+
+    }
+
+
+
+  
+
+    
+
+    
+
+}
