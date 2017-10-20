2ce042abbd669786551fff65630441422a298c2c
ManuTaken <32471334+ManuTaken@users.noreply.github.com>
2017-10-20 16:03:54 -0200

    Create IngressoNormal.java

---
+++ IngressoNormal.java
@@ -0,0 +1,28 @@
+# Aula01
+
+
+public class Normal extends Ingresso{
+
+    
+
+    public void valorNormal()
+
+    {
+
+        System.out.println("Valor Normal: R$"+valor);
+
+          
+
+    }
+
+    
+
+    
+
+    
+
+    
+
+    
+
+}
