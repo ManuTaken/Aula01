9718e32447900a55f2168c33645e8f1ba032c675
ManuTaken <32471334+ManuTaken@users.noreply.github.com>
2017-10-20 16:01:25 -0200

    Create Ingresso.java

---
+++ Ingresso.java
@@ -0,0 +1,27 @@
+# Aula01
+
+public class Ingresso {
+
+    
+
+    float valor=20;
+
+    
+
+    public void imprimeValor()
+
+    {
+
+        System.out.println("ingresso: R$"+valor);
+
+    
+
+    }
+
+    
+
+    
+
+    
+
+}
