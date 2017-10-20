08227ebdb03f62335e433153a7cde20c23dca227
ManuTaken <32471334+ManuTaken@users.noreply.github.com>
2017-10-20 16:06:05 -0200

    Create CamaroteSup.java

---
+++ CamaroteSup.java
@@ -0,0 +1,72 @@
+# Aula01
+
+
+public class CamaroteSuperior extends Vip {
+
+    
+
+   String localizacao="Camarote Superior";
+
+   float taxa=25;
+
+   float camSup;
+
+
+
+    public String getLocalizacaoS() {
+
+        return localizacao;
+
+    }
+
+
+
+    public void setLocalizacaoS(String localizacao) {
+
+        this.localizacao = localizacao;
+
+    }
+
+
+
+    public float getCamSup() {
+
+        return camSup;
+
+    }
+
+
+
+    public void setCamSup(float camSup) {
+
+        this.camSup = camSup;
+
+    }
+
+   
+
+    
+
+    public void camaroteSup()
+
+    {
+
+        this.camSup= taxa + valor + adicional;
+
+            
+
+        System.out.println("Valor do ingresso: R$"+camSup);
+
+        System.out.println("Localização do seu Ingresso: "+ localizacao);
+
+    }
+
+        
+
+    
+
+    
+
+    
+
+}
