5c2cc6e262d03410fa253d4ff792b32ebc5fa154
ManuTaken <32471334+ManuTaken@users.noreply.github.com>
2017-10-20 16:05:02 -0200

    Create CamaroteInf.java

---
+++ CamaroteInf.java
@@ -0,0 +1,53 @@
+# Aula01  
+
+public class CamaroteInferior extends Vip {
+
+    
+
+    String localizacao="Camarote Inferior";
+
+    float camInf= valor+adicional;
+
+    
+
+    
+
+    public String getLocalizacao() {
+
+        return localizacao;
+
+    }
+
+
+
+    public void setLocalizacao(String localizacao) {
+
+        this.localizacao = localizacao;
+
+    }
+
+       
+
+       
+
+    
+
+    public void camaroteInf()
+
+    {
+
+        System.out.println("Valor do Ingresso: R$"+ camInf);
+
+        System.out.println("Localização do seu Ingresso: "+ localizacao);
+
+    
+
+    }
+
+    
+
+    
+
+    
+
+}
